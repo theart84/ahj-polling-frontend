e50d222805052f7b471b0d1153bf91c28603608f
theart84 <artshadow74@gmail.com>
2021-05-23 01:52:39 +0500

    catchError fixed

--- src/js/Widget.js
+++ src/js/Widget.js
@@ -36,7 +36,11 @@ export default class Widget {
               return filteredResponse;
             }),
             timestamp(),
-            catchError(() => of([]))
+            catchError(() =>
+              of({
+                value: [],
+              })
+            )
           )
         )
       )
